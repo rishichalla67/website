9cbf1a7f77d56a362168bb21a800b36a0db0063e
rishichalla67 <rishichalla@ufl.edu>
2020-08-29 21:41:59 -0400

    Added portfolio page

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './Portfolio.css'
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 export default function Portfolio() {
     return(
@@ -8,8 +9,12 @@ export default function Portfolio() {
             <div className="box">
                 <a href="https://rishi-challa-hulu-clone.web.app/" target="blank">Hulu UI Clone</a>
                 <p></p>
-                <a href="https://github.com/rishichalla67/hulu-clone" target="blank"> Github </a>
-
+                <h3><a href="https://github.com/rishichalla67/hulu-clone" target="blank"><GitHubIcon/></a></h3>
+            </div>
+            <div className="box">
+                <a href="https://rishi-challa-airbnb-clone.web.app/" target='blank'>Airbnb UI Clone</a>
+                <p></p>
+                <h3><a href="https://github.com/rishichalla67/airbnb_clone" target="blank"><GitHubIcon/></a></h3>
             </div>
         </div>
     );
