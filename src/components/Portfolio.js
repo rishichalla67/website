2964f6c6b3340ff4ce46a5309c7003343dd5c51a
rishichalla67 <rishichalla@ufl.edu>
2020-08-29 22:21:19 -0400

    Added portfolio page

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,11 +1,23 @@
 import React from "react";
 import './Portfolio.css'
 import GitHubIcon from '@material-ui/icons/GitHub';
+import HomeIcon from '@material-ui/icons/Home'
+import {Link} from 'react-router-dom'
+
 
 export default function Portfolio() {
     return(
         <div className='Portfolio'>
-            <h1>Rishi's Coding Projects</h1>
+
+            <div className="title">
+                <h2>
+                    <Link to='/'>
+                        <HomeIcon />
+                    </Link>
+                </h2>
+                <h1> Rishi's Coding Projects</h1>
+            </div>
+
             <div className="box">
                 <a href="https://rishi-challa-hulu-clone.web.app/" target="blank">Hulu UI Clone</a>
                 <p></p>
