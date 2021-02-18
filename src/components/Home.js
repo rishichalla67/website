9aab7ea3ef419bd677b62058556703d00fa83804
rishichalla67 <36481964+rishichalla67@users.noreply.github.com>
2021-02-18 10:33:12 -0400

    'navbar'

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './home.css'
+import Divider from '@material-ui/core/Divider';
 import linkedin from './linkedin.png';
 import github from './github.png';
 import email from './gmail.png';
-import { MenuList, MenuItem, Button } from '@material-ui/core';
+import { Box, MenuList, MenuItem, Button } from '@material-ui/core';
 import Portfolio from "./Portfolio";
 import {Link} from "react-router-dom";
-import { ChakraProvider, useColorMode, useColorModeValue, Box } from "@chakra-ui/react"
 import Brightness7Icon from '@material-ui/icons/Brightness7';
 import Brightness4OutlinedIcon from '@material-ui/icons/Brightness4Outlined';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
@@ -15,60 +15,22 @@ import MailOutlineIcon from '@material-ui/icons/MailOutline';
 
 
 
+
 export default function Home() {
-    const { colorMode, toggleColorMode } = useColorMode();
-    const bg = useColorModeValue("blue.200", "black");
-    const text = useColorModeValue("black.800", "white");
     return(
-        <ChakraProvider>
-            <Box w="100%" h="100vh" bg={bg} color={text} >
-                <div className='bg' >
-                    <div className='back'></div>
-                    <div className='main'>
-
-                        <nav className='menu'>
-                            <ul className='navbar'>
-                                {/*<li><a href='#About'>About</a></li>*/}
-                                <li><a color={text} href="https://docs.google.com/document/u/1/d/e/2PACX-1vR0-_ctgqtTl26HuxAIRV12OmANYFHgfS5-QBgfc-yohFdgidSdx2QcDOqFPzIR-Tg1ofNjuEm5iAp_/pub" target="_blank">Resumė</a></li>
-                                {/*<li><a href='#Contact'>Contact</a></li>*/}
-                                <li><a color={text} href='https://secure.actblue.com/donate/ms_blm_homepage_2019' target="_blank"></a></li>
-                                <li><a color={text}>
-                                    <Button onClick={toggleColorMode}>
-                                        {colorMode === "light" ? <Brightness4OutlinedIcon/> : <Brightness7Icon style={{fill: "white"}}/>}
-                                    </Button>
-                                </a></li>
-
-                            </ul>
-                        </nav>
-                        <div className='body'>
-                            <h1 color={text}>Hey, I'm Rishi Challa</h1>
-                            <h2><Link to='/Portfolio' style={{textDecoration: 'none', color: {text}, }}> Coding Projects </Link></h2>
-                        </div>
-                    </div>
-                    <div className='back'></div>
-                    <Box w="50%" h="50vh" className='main'>
-                        {/*<div id='About'>*/}
-                        {/*    <h2>Who Am I?</h2>*/}
-                        {/*    <a>Currently a University of Florida Computer Science Undergraduate Senior</a>*/}
-                        {/*    <a>Interests: Web Design and App Development</a>*/}
-                        {/*    <a>Hobbies: Playing sports and video games</a>*/}
-                        {/*    <a className='link' href="https://docs.google.com/document/d/e/2PACX-1vQSOCDVCV_YbrB1KyDhdKNONmSOLaW64JMBg_o5k89yvveSHFOmzuQoKuIovXlQBg9RZJDLeW7Pzelf/pub" target="_blank"><button className='button'>Resumė</button></a>*/}
-                        {/*</div>*/}
-                    </Box>
-                    {/*<div className='Contact'>*/}
-                    {/*    <div id='Contact'>*/}
-                    {/*        <h2>Contact Me!</h2>*/}
-                    {/*        <p>I am available for freelance website building, feel free to send me an email!</p>*/}
-                    {/*        <ul>*/}
-                    {/*            <li><a href='https://www.linkedin.com/in/rishi-challa-7802b8179/' target="_blank"><img src={linkedin}/></a></li>*/}
-                    {/*            <li><a href='https://github.com/rishichalla67' target="_blank"><img src={github}/></a></li>*/}
-                    {/*            <li><a href='mailto:rishichalla67@gmail.com' target="_blank"><img src={email}/></a></li>*/}
-                    {/*        </ul>*/}
-
-                    {/*    </div>*/}
-                    {/*</div>*/}
+        <div style={{backgroundColor: 'lightcyan', height:"100vh"}} className='container'>
+            <div className="box">
+                <div className='navbar'>
+                    <h3>Resumé</h3>
+                    <h1>Rishi Challa</h1>
+                    <ul>
+                        <a href='https://www.linkedin.com/in/rishi-challa-7802b8179/'><LinkedInIcon /></a>
+                        <a href='https://github.com/rishichalla67'><GitHubIcon /></a>
+                        <a href='mailto:rishichalla67@gmail.com'><MailOutlineIcon /></a>
+                    </ul>
                 </div>
-            </Box>
-        </ChakraProvider>
+                <hr />
+            </div>
+        </div>
     );
 }
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,31 +3,55 @@ import './Portfolio.css'
 import GitHubIcon from '@material-ui/icons/GitHub';
 import HomeIcon from '@material-ui/icons/Home'
 import {Link} from 'react-router-dom'
+import { Box } from "@chakra-ui/react"
+import {Divider, HStack, Center, Square, Circle } from "@chakra-ui/react"
 
 
 export default function Portfolio() {
     return(
         <div className='Portfolio'>
+            <Center>
+                <Box className="projectBox" w="50%" p="10" borderWidth="5px" borderRadius="lg" >
+                    <div className="title">
+                        {/*<h2>*/}
+                        {/*    <Link to='/'>*/}
+                        {/*        <HomeIcon />*/}
+                        {/*    </Link>*/}
+                        {/*</h2>*/}
+                        <h1>Projects</h1>
+                    </div>
 
-            <div className="title">
-                <h2>
-                    <Link to='/'>
-                        <HomeIcon />
-                    </Link>
-                </h2>
-                <h1> Rishi's Coding Projects</h1>
-            </div>
-
-            <div className="box">
-                <a href="https://rishi-challa-hulu-clone.web.app/" target="blank">Hulu UI Clone</a>
-                <p></p>
-                <h3><a href="https://github.com/rishichalla67/hulu-clone" target="blank"><GitHubIcon/></a></h3>
-            </div>
-            <div className="box">
-                <a href="https://rishi-challa-airbnb-clone.web.app/" target='blank'>Airbnb UI Clone</a>
-                <p></p>
-                <h3><a href="https://github.com/rishichalla67/airbnb_clone" target="blank"><GitHubIcon/></a></h3>
-            </div>
+                    <div className="box">
+                        <Center>
+                            <HStack spacing="300px" paddingTop="50px">
+                                <a href="https://rishi-challa-hulu-clone.web.app/" target="blank">Hulu UI Clone</a>
+                                <p></p>
+                                <h3><a href="https://github.com/rishichalla67/hulu-clone" target="blank"><GitHubIcon/></a></h3>
+                            </HStack>
+                        </Center>
+                    </div>
+                    <Divider borderWidth="3px" color="teal.600"/>
+                    <div className="box">
+                        <Center>
+                            <HStack spacing="295px" paddingTop="50px">
+                                <a href="https://rishi-challa-airbnb-clone.web.app/" target='blank'>Airbnb UI Clone</a>
+                                <p></p>
+                                <h3><a href="https://github.com/rishichalla67/airbnb_clone" target="blank"><GitHubIcon/></a></h3>
+                            </HStack>
+                        </Center>
+                    </div>
+                    <Divider borderWidth="3px" bg="#FFFFFF"/>
+                    <div className="box">
+                        <Center>
+                            <HStack spacing="180px" paddingTop="50px">
+                                <a>Python Quantum Computing Educational Game</a>
+                                <p></p>
+                                <h3><a href="https://github.com/mfarina1/qwack" target="blank"><GitHubIcon/></a></h3>
+                            </HStack>
+                        </Center>
+                    </div>
+                </Box>
+            </Center>
         </div>
     );
 }
